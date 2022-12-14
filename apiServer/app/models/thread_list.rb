a239c3c97d09827352c8f40f4513c26c021cca68
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2022-12-14 17:28:52 +0800

    Update thread_list.rb

--- apiServer/app/models/thread_list.rb
+++ apiServer/app/models/thread_list.rb
@@ -1,5 +1,5 @@
 class ThreadList < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy  
 
     validates :title, presence: true
     validates :desc, presence: true
